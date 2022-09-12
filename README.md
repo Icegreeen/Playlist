fdd82ea58fa4a9b56474276b5d8f31790cc797aa
icegreeen1 <93l7m7am0jjwjx0dxf6tsk1n3>
2022-09-12 04:05:04 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+<h1 align="center">
+    <a">🔗 Playlist</a>
+</h1>
+<p align="center">🚀 A playlist for courses.</p>
+
+> Project status: Completed :heavy_check_mark: [Click here to view!](https://resplendent-liger-a99762.netlify.app/)
+
+## ![bg(2)](https://user-images.githubusercontent.com/56550632/167311962-f801970e-f9e6-4987-bcb8-58fec7e5f60a.jpg)
---
+++ videos.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    src: "./img/video1.mp4",
+    title: "Getting started",
+  },
+  {
+    src: "./img/video2.mp4",
+    title: "Html | Css | JavaScript ",
+  },
+  {
+    src: "./img/video3.mp4",
+    title: "React and Typescript",
+  },
+  {
+    src: "./img/video4.mp4",
+    title: "MySQL and GraphQL",
+  },
+  {
+    src: "./img/video5.mp4",
+    title: "Next | Vue | Gatsby | Styld-components",
+  },
+  {
+    src: "./img/video6.mp4",
+    title: "Build and Deploy",
+  },
+];
